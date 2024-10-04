25b00023eb32550e24056cf83531d87659684456
Paradva-Niraj <nirajparadva2004@gmail.com>
2024-10-05 00:09:23 +0530

    font

---
+++ userfile/user_header.php
@@ -0,0 +1,54 @@
+<div class="logo">
+    <div class="logo-img">
+        <img src="./projectimg/icon.png" alt="" srcset="">
+    </div>
+    <div class="logo-name">
+        <h3>book motion library services</h3>
+    </div>
+    <div class="logout">
+        <a href="logout.php" class="btn">log me out</a>
+    </div>
+</div>
+<hr />
+<div class="navigation">
+<ul class="lin">
+            <li><a href="student_dashboard.php">dashboard</a></li>
+            <li><a href="books.php">isssued books</a></li>
+            <div class="dropdown">
+                <li><a>account</a></li>
+                <div class="dropdown-manu d2" id="d2">
+                    <li><a href="profile.php">my profile</a></li>
+                    <li><a href="changepassword.php">password</a></li>
+                    <li><a href="./thought.php">thought</a></li>
+                </div>
+            </div>
+        </ul>
+</div>
+<hr />
+<div class="logo">
+    <div class="logo-img">
+        <img src="./projectimg/icon.png" alt="" srcset="">
+    </div>
+    <div class="logo-name">
+        <h3>book motion library services</h3>
+    </div>
+    <div class="logout">
+        <a href="logout.php" class="btn">log me out</a>
+    </div>
+</div>
+<hr />
+<div class="navigation">
+<ul class="lin">
+            <li><a href="student_dashboard.php">dashboard</a></li>
+            <li><a href="books.php">isssued books</a></li>
+            <div class="dropdown">
+                <li><a>account</a></li>
+                <div class="dropdown-manu d2" id="d2">
+                    <li><a href="profile.php">my profile</a></li>
+                    <li><a href="changepassword.php">password</a></li>
+                    <li><a href="./thought.php">thought</a></li>
+                </div>
+            </div>
+        </ul>
+</div>
+<hr />
